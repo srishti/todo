d51c66692f65aaba97c5298a8e157176129ecf3d
Srishti Gupta <mail@srishtigupta.in>
2021-06-07 12:20:43 +0530

    added Tailwind for styling

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,13 +8,16 @@ export default function TodoList() {
     state.todos.length > 0 ? `${state.todos.length} TODOs` : "Nothing To Do!";
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <ul>
+    <div className="container mx-auto max-w-md text-center font-mono">
+      <h1 className="text-bold mt-5">{title}</h1>
+      <ul className="list-reset text-white p-0">
         {state.todos.map((todo) => {
           return (
-            <li key={todo.id}>
-              <span>{todo.text}</span>
+            <li
+              key={todo.id}
+              className="flex items-center bg-orange-dark border-black border-dashed border-2 my-2 py-4"
+            >
+              <span className="flex-1 cursor-pointer">{todo.text}</span>
             </li>
           );
         })}
